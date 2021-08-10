847902edf89c44096f59b596b942fa3552696133
Daniel Viveros <dviveros90@gmail.com>
2021-08-10 15:04:34 -0400

    6. Scenario (Without DI)

--- Dependency.Injection/Program.cs
+++ Dependency.Injection/Program.cs
@@ -2,11 +2,62 @@
 
 namespace Dependency.Injection
 {
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            this.id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            this.log.Write($"Engine [{id}] ahead {power}.");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            this.engine.Ahead(100);
+            this.log.Write("Car going foward...");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(engine, log);
+            car.Go();
         }
     }
 }
