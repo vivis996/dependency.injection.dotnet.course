585d7f51b47057ceecbdcb4d806bd62d075c82b1
Daniel Viveros <dviveros90@gmail.com>
2023-09-20 11:54:16 -0400

    Captive Dependencies

--- Dependency.Injection/Program.cs
+++ Dependency.Injection/Program.cs
@@ -1,8 +1,55 @@
+using System.Collections.Generic;
+using Autofac;
+
 namespace Dependency.Injection;
+
+public interface IResource
+{
+}
+
+public class SingletonResource : IResource
+{
+}
+
+public class InstancePerDependencyResource : IResource, IDisposable
+{
+    public InstancePerDependencyResource()
+    {
+        Console.WriteLine("Instance per dep created");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Instance per dep destroyed");
+    }
+}
+
+public class ResourceManager
+{
+    public IEnumerable<IResource> Resources { get; set; }
+
+    public ResourceManager(IEnumerable<IResource> resources)
+    {
+        Resources = resources;
+    }
+}
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<ResourceManager>().SingleInstance();
+        builder.RegisterType<SingletonResource>()
+               .As<IResource>()
+               .SingleInstance();
+        builder.RegisterType<InstancePerDependencyResource>()
+               .As<IResource>();
+
+        using (var container = builder.Build())
+        using(var scope = container.BeginLifetimeScope())
+        {
+            scope.Resolve<ResourceManager>();
+        }
     }
 }
