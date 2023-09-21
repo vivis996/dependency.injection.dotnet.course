40edf46907a2a65bd1fc1ea0dafb2ec4dc4bfc63
Daniel Viveros <dviveros90@gmail.com>
2023-09-21 19:21:12 -0400

    Registration Sources (before)

--- Dependency.Injection/Program.cs
+++ Dependency.Injection/Program.cs
@@ -1,61 +1,15 @@
-using System.Collections.Generic;
-using System.IO;
 using Autofac;
-using Autofac.Configuration;
-using Microsoft.Extensions.Configuration;
 
 namespace Dependency.Injection;
-
-public interface IOperation
-{
-    float Calculate(float a, float b);
-}
-
-public class Addition : IOperation
-{
-    public float Calculate(float a, float b)
-    {
-        return a + b;
-    }
-}
-
-public class Multiplication : IOperation
-{
-    public float Calculate(float a, float b)
-    {
-        return a * b;
-    }
-}
-
-public class CalculationModule : Module
-{
-    protected override void Load(ContainerBuilder builder)
-    {
-        builder.RegisterType<Addition>().As<IOperation>();
-        builder.RegisterType<Multiplication>().As<IOperation>();
-    }
-}
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var configBuilder = new ConfigurationBuilder()
-                            .SetBasePath(Directory.GetCurrentDirectory())
-                            .AddJsonFile("config.json");
-        var configuration = configBuilder.Build();
-
         var containerBuilder = new ContainerBuilder();
-        var configModule = new ConfigurationModule(configuration);
-        containerBuilder.RegisterModule(configModule);
 
         using (var container = containerBuilder.Build())
         {
-            float a = 3, b = 4;
-            foreach (var op in container.Resolve<IList<IOperation>>())
-            {
-                Console.WriteLine($"{op.GetType().Name} of {a} and {b} = {op.Calculate(a, b)}");
-            }
         }
     }
 }
