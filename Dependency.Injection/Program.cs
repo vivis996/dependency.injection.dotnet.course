41f7fea9aa48db65387d43effe7b57d9559b4f0d
Daniel Viveros <dviveros90@gmail.com>
2023-09-21 19:03:54 -0400

    JSON/XML Configuration with Microsoft Configuration

--- Dependency.Injection/Program.cs
+++ Dependency.Injection/Program.cs
@@ -1,8 +1,52 @@
+using System.Collections.Generic;
+using System.IO;
+using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+
 namespace Dependency.Injection;
+
+public interface IOperation
+{
+    float Calculate(float a, float b);
+}
+
+public class Addition : IOperation
+{
+    public float Calculate(float a, float b)
+    {
+        return a + b;
+    }
+}
+
+public class Multiplication : IOperation
+{
+    public float Calculate(float a, float b)
+    {
+        return a * b;
+    }
+}
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var configBuilder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("config.json");
+        var configuration = configBuilder.Build();
+
+        var containerBuilder = new ContainerBuilder();
+        var configModule = new ConfigurationModule(configuration);
+        containerBuilder.RegisterModule(configModule);
+
+        using (var container = containerBuilder.Build())
+        {
+            float a = 3, b = 4;
+            foreach (var op in container.Resolve<IList<IOperation>>())
+            {
+                Console.WriteLine($"{op.GetType().Name} of {a} and {b} = {op.Calculate(a, b)}");
+            }
+        }
     }
 }
